d651226ac3267afb77bd82b86a6702f6c9de1a2f
jinu0137 <ljo2379@gmail.com>
2021-12-05 01:08:22 +0900

    feat : InheritanceEx

--- ClassEx.py
+++ ClassEx.py
@@ -8,15 +8,15 @@ class Horse:
         self.velocity = 0
 
 class Alphabets:
-    __str = ""
-    def __init__(self, text):
+    __str = ""                          # 내부 멤버변수
+    def __init__(self, text):           # 객체 생성자
         self.text = text
         Alphabets.__str += text
     def print_class_variable(self):
         print(Alphabets.__str)
 
-if __name__ == '__main__':
-    o1 = Alphabets('p')
+if __name__ == '__main__':              # 현재 파일 안에서 실행되는 경우 : __main__ / import해서 다른 파일에서 실행되는 경우 ClassEx
+    o1 = Alphabets('p')                 # 6개의 객체가 생성되는 과정에서 Alphabets 클래스의 클래스 변수인 str이 서로 공유
     o2 = Alphabets('y')
     o3 = Alphabets('t')
     o4 = Alphabets('h')
---
+++ InheritanceEx.py
@@ -0,0 +1,39 @@
+import math
+
+class Animal:
+    def __init__(self, age, height, color, xpos, ypos):
+        self.age = age
+        self.height = height
+        self.color = color
+        self.xposition = xpos
+        self.yposition = ypos
+        self.velocity = 0
+
+    def sound(self):
+        pass    # 실행할 코드가 없음을 의미(문법적으로 문장이 필요하지만, 프로그램이 할 일이 없을 때 사용), Java의 Interface와 같이 의무적으로 상속시킬 때 사용하는 듯
+
+class Horse(Animal):
+    def __init__(self, age, height, color, xpos, ypos):
+        Animal.__init__(self, age, height, color, xpos, ypos)           # 부모 클래스(Animal)의 생성자 호출
+
+    def sound(self):
+        print('Neigh')
+
+    def run(self, xdistance, ydistance, time):
+        self.xposition += xdistance
+        self.yposition += ydistance
+        total_distance = math.sqrt((xdistance + xdistance) * (ydistance + ydistance))
+        self.velocity = total_distance/time
+
+class Dog(Animal):
+    def __init__(self, age, height, color, xpos, ypos):
+        Animal.__init__(self, age, height, color, xpos, ypos)
+
+    def sound(self):
+        print('Bow-Wow')
+
+if __name__ == '__main__':
+    danbi = Horse(5, 160, 'brown', 0, 0)
+    choco = Dog(10, 100, 'black', 50, 30)
+    danbi.sound()
+    choco.sound()
